28092338e05999c391063b1c313b1669721f9895
shanteljackson <128184637+shanteljackson@users.noreply.github.com>
2023-12-16 09:26:10 -0800

    V3_roll_die.py

--- original_roll_die.py
+++ original_roll_die.py
@@ -4,19 +4,27 @@ def roll_dice(number_of_dice):
     return [random.randint(1, 6) for _ in range(number_of_dice)]
 
 def main():
-    try:
-        num_dice = int(input("Enter the number of dice to roll: "))
-        if num_dice <= 0:
-            raise ValueError("Number of dice must be a positive integer.")
-        
-        print(f"Rolling {num_dice} dice...")
-        results = roll_dice(num_dice)
-        print("Results:", results)
+    print("Welcome to the Dice Rolling Program!")
     
-    except ValueError as ve:
-        print(f"Error: {ve}")
-    except KeyboardInterrupt:
-        print("\nDice rolling aborted by user.")
+    while True:
+        try:
+            num_dice = int(input("Enter the number of dice to roll (or enter 0 to exit): "))
+            
+            if num_dice == 0:
+                print("Goodbye!")
+                break
+            
+            if num_dice < 0:
+                raise ValueError("Number of dice must be a positive integer.")
+            
+            print(f"Rolling {num_dice} dice...")
+            results = roll_dice(num_dice)
+            print("Results:", results)
+        
+        except ValueError as ve:
+            print(f"Error: {ve}")
+        except KeyboardInterrupt:
+            print("\nDice rolling aborted by user.")
 
 if __name__ == "__main__":
     main()
